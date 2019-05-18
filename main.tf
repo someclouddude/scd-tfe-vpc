849cd877d85d3490a4cd0b8dfc4997b26f50304a
Ian Evans <someclouddude@gmail.com>
2019-05-18 15:50:40 -0500

    fixed key

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "vault_generic_secret" "creds" {
 }
 
 provider "aws" {
-  access_key = "${data.vault_generic_secret.creds.data[access_key_id]}"
-  secret_key = "${data.vault_generic_secret.creds.data[secret_access_key]}"
+  access_key = "${data.vault_generic_secret.creds.data["access_key_id"]}"
+  secret_key = "${data.vault_generic_secret.creds.data["secret_access_key"]}"
   region = "us-east-1"
 }
